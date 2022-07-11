ab452746b8bab1079b7aebe28aea1f0c332aaa3c
KevinEstrada12 <43767301+KevinEstrada12@users.noreply.github.com>
2022-07-10 22:22:28 -0500

    Adding all the files needed to start submitting the questions including the EvenOdd Kyu

---
+++ src/kata/kyu8/EvenOrOdd.java
@@ -0,0 +1,12 @@
+package kata.kyu8;
+
+public class EvenOrOdd {
+	    public static String even_or_odd(int number) {
+	        if(number % 2 == 0){
+	          return "Even";
+	        } else {
+	          return "Odd";
+	        }
+	        
+	    }
+	}
